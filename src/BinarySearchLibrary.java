a55dca8ad32c83c5b94b99b9ca08ea12e81ec90b
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2018-11-19 13:41:04 -0500

    one fewer comparison

--- src/BinarySearchLibrary.java
+++ src/BinarySearchLibrary.java
@@ -34,7 +34,7 @@ public class BinarySearchLibrary {
 		int high = list.size()-1;
 		if (list.size() == 0) return -1;
 		while (low+1 != high) {
-			if (comp.compare(list.get(low+1), target) == 0) return low+1;
+//			if (comp.compare(list.get(low+1), target) == 0) return low+1;
 			mid = (low + high)/2;
 //			if (list.subList(low+1, mid+1).contains(target)) {
 //				high = mid;
@@ -44,7 +44,6 @@ public class BinarySearchLibrary {
 //			}
 	        if (comp.compare(target, list.get(mid)) <= 0)
 	            high = mid;
-//	        else if (comp.compare(target, list.get(mid)) > 0)
 	        else low = mid;
 		}
 		if (comp.compare(list.get(low+1), target) == 0) return low+1;
@@ -68,7 +67,7 @@ public class BinarySearchLibrary {
 		int high = list.size();
 		if (list.size() == 0) return -1;
 		while (low+1 != high) {
-			if (comp.compare(list.get(high-1), target) == 0) return high-1;
+//			if (comp.compare(list.get(high-1), target) == 0) return high-1;
 			mid = (low + high)/2;
 //			if (list.subList(mid, high).contains(target)) {
 //				low = mid;
@@ -78,7 +77,6 @@ public class BinarySearchLibrary {
 //			}
 	        if (comp.compare(target, list.get(mid)) >= 0)
 	            low = mid;
-//	        else if (comp.compare(target, list.get(mid)) < 0)
 	        else high = mid;
 		}
 		if (comp.compare(list.get(high-1), target) == 0) return high-1;
